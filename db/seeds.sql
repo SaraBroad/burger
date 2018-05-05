664302b2d92ab6bf0ed2e6c86486394034b8913d
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-04 23:00:02 -0400

    db

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Donatello', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Leonardo', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Michaelangelo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Raphael', true);
