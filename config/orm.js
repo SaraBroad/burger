8bf8261a01ea2e253c650956f6b40c7c83353b0f
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-06 19:42:29 -0400

    functions

--- config/orm.js
+++ config/orm.js
@@ -2,25 +2,25 @@ var connection = require("../config/connection.js");
 
 var orm = {
     selectAll: function () {
-
-        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+        connection.query("SELECT * FROM burgers", function (err, result) {
             if (err) throw err;
             console.log(result);
         });
     },
-    insertOne: function () {
-        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+    insertOne: function (burger_name) {
+        var queryString = "INSERT INTO burgers WHERE burger_name = ?"
+        connection.query(queryString, [burger_name], function (err, result) {
             if (err) throw err;
             console.log(result);
         });
     },
     updateOne: function () {
+        // var queryString = "SELECT * FROM ?? WHERE ?? = ?";
         connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
             if (err) throw err;
             console.log(result);
         });
     }
-
 }
 
 module.exports = orm;
