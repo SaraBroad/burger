dd8a85934d862fec0d06e0f36d7c9626a0d50d8c
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-06 16:57:10 -0400

    orm

--- config/orm.js
+++ config/orm.js
@@ -1,8 +1,26 @@
 var connection = require("../config/connection.js");
 
-selectAll();
-insertOne();
-updateOne;
+var orm = {
+    selectAll: function () {
 
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    insertOne: function () {
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    },
+    updateOne: function () {
+        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+            if (err) throw err;
+            console.log(result);
+        });
+    }
+
+}
 
 module.exports = orm;
