ed4bbc7359f719252340a5f3ebac2bc4cc0e67b7
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-07 16:50:42 -0400

    burger.js

--- models/burger.js
+++ models/burger.js
@@ -2,7 +2,23 @@ var orm = require("../config/orm.js");
 
 
 var burger = {
-
+    selectAll: function (func) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+    insertOne: function (cols, vals, func) {
+        orm.insertOne("burgers", cols, vals, function (res) {
+            func(res);
+        });
+    },
+    updateOne: function (objsColsVals, condition, func) {
+        orm.updateOne("burgers", objsColsVals, condition, function (res) {
+            func(res);
+        });
+    }
 }
 
 module.exports = burger;
+
+
