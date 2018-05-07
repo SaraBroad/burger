e8c32051115d6526e467dfdd73e2220c01928b4e
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-06 22:13:32 -0400

    orm

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,56 @@
 var connection = require("../config/connection.js");
 
 var orm = {
-    selectAll: function () {
-        connection.query("SELECT * FROM burgers", function (err, result) {
+    selectAll: function (table_name, func) {
+        var queryString = "SELECT * FROM ??";
+        connection.query(queryString, [table_name], function (err, result) {
             if (err) throw err;
             console.log(result);
+            func(result);
         });
     },
-    insertOne: function (burger_name) {
-        var queryString = "INSERT INTO burgers WHERE burger_name = ?"
-        connection.query(queryString, [burger_name], function (err, result) {
+    insertOne: function (table_name, cols, vals, func) {
+        var queryString = "INSERT INTO " + table_name;
+        queryString += " (" + cols.join(', ') + ") ";
+        queryString += "VALUES (" + getQues(vals.length) + ")"
+        connection.query(queryString, vals, function (err, result) {
             if (err) throw err;
             console.log(result);
+            func(result);
         });
     },
-    updateOne: function () {
+
+    updateOne: function (table_name, cols, vals, func) {
         // var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-        connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
-            if (err) throw err;
-            console.log(result);
-        });
+        var queryString = "UPDATE " + table_name;
+        queryString +=
+            connection.query(queryString, [tableInput, colToSearch, valOfCol], function (err, result) {
+                if (err) throw err;
+                console.log(result);
+                func(result);
+            });
     }
+}
+
+function getQues(number) {
+    var quesArray = [];
+    for (var i = 0; i < number.length; i++) {
+        quesArray.push("?");
+    }
+    return quesArray.join(", ");
+}
+
+function objToSql(ob) {
+    for (var key in ob) {
+        var array = [];
+        var value = ob[key];
+        if (Object.hasOwnProperty.call(ob, key)) {
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+        }
+    }
+
 }
 
 module.exports = orm;
