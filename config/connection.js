257ea6fa2f0d01f7b43986331f5229e7126d631d
Sara Broad <sarabroad@Saras-MacBook-Pro-2.local>
2018-05-12 16:06:43 -0400

    express update

--- config/connection.js
+++ config/connection.js
@@ -1,9 +1,7 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
+
 var connection = mysql.createConnection({
   port: 3306,
   host: "localhost",
@@ -11,7 +9,7 @@ var connection = mysql.createConnection({
   password: "Fiction1912",
   database: "burgers_db"
 });
-};
+
 
 // Make connection.
 connection.connect(function(err) {
